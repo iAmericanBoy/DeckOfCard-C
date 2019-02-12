cbcc9154341fb159343329ac0b19071f3f7090de
iAmericanBoy <32912858+iAmericanBoy@users.noreply.github.com>
2019-02-12 14:00:17 -0700

    Initial Commit

---
+++ DeckOfCard-C/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DeckOfCard-C
+//
+//  Created by Dominic Lanzillotta on 2/12/19.
+//  Copyright © 2019 Dominic Lanzillotta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
